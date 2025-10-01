bc4aabf3413076524cbdaa22f21a259b8a702af1
lyapkin <lyapkin992@gmail.com>
2025-10-01 16:35:38 +0500

    refactor button ui

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -2,16 +2,20 @@ import { HTMLAttributes } from "react";
 import s from "./styles.module.scss";
 import clsx from "clsx";
 
-const Button = ({
-  className,
-  children,
-  ...rest
-}: HTMLAttributes<HTMLButtonElement>) => {
+const Button = ({ className, type = "primary", children, ...rest }: Props) => {
   return (
-    <button className={clsx(s.button, className)} {...rest}>
+    <button
+      className={clsx(s.button, s[`button__${type}`], className)}
+      {...rest}
+    >
       {children}
     </button>
   );
 };
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+  type?: "primary" | "secondary";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
 
 export default Button;
